09a014764d1fcdffa14dbfe4fdb55aff4dd1b78a
Zafir Stojanovski <zaf.stojano@gmail.com>
2025-03-03 00:38:17 +0100

    spiral matrix fixed

--- reasoning_gym/algorithmic/spiral_matrix.py
+++ reasoning_gym/algorithmic/spiral_matrix.py
@@ -12,6 +12,13 @@ from ..factory import ProceduralDataset, register_dataset
 
 QUESTION_TEMPLATE = """Given a matrix, your job is to generate a list of elements in spiral order, starting from the top-left element.
 
+The spiral order is clockwise, starting from the top-left corner. More precisely:
+- Start from the top-left corner and move right.
+- Move down towards the bottom-right corner.
+- Move left towards the bottom-left corner.
+- Move up towards the top-right corner.
+- Repeat the steps for the inner elements of the matrix until every entry is visited.
+
 Your output should be a space-separated list of integers, e.g. 1 2 3 4 5 6
 
 For the matrix below, what is the list of elements in spiral order?
