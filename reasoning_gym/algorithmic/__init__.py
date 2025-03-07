a2fa7535c1766ae3d40c8fec13bf3fc89b6a680c
Zafir Stojanovski <zaf.stojano@gmail.com>
2025-03-07 22:58:04 +0100

    rotate matrix curriculum

--- reasoning_gym/algorithmic/__init__.py
+++ reasoning_gym/algorithmic/__init__.py
@@ -28,7 +28,7 @@ from .palindrome_generation import PalindromeConfig, PalindromeDataset
 from .palindrome_partitioning import PalindromePartitioningConfig, PalindromePartitioningDataset
 from .pool_matrix import PoolMatrixConfig, PoolMatrixDataset
 from .ransom_note import RansomNoteConfig, RansomNoteDataset
-from .rotate_matrix import RotateMatrixConfig, RotateMatrixDataset
+from .rotate_matrix import RotateMatrixConfig, RotateMatrixCurriculum, RotateMatrixDataset
 from .rotten_oranges import RottenOrangesConfig, RottenOrangesDataset
 from .sentence_reordering import SentenceReorderingConfig, SentenceReorderingDataset
 from .spell_backward import SpellBackwardConfig, SpellBackwardDataset
@@ -85,6 +85,7 @@ __all__ = [
     "IsomorphicStringsDataset",
     "RotateMatrixConfig",
     "RotateMatrixDataset",
+    "RotateMatrixCurriculum",
     "ManipulateMatrixConfig",
     "ManipulateMatrixDataset",
     "BinaryMatrixConfig",
