c38810030fad5d0e28bdc5cc5c79fb23256eef0f
joesharratt1229 <joesharratt1229@gmail.com>
2025-03-10 06:52:21 +0000

    added emoji curriculum

--- reasoning_gym/games/emoji_mystery.py
+++ reasoning_gym/games/emoji_mystery.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 from random import Random
 from typing import Any, Optional
 
+from ..coaching import AttributeType, BaseCurriculum, RangeAttributeDefinition
 from ..data import read_data_file
 from ..factory import ProceduralDataset, register_dataset
 
@@ -231,6 +232,24 @@ class EmojiMysteryDataset(ProceduralDataset):
             except:
                 reward = 0.01
         return reward
+
+
+class EmojiMysteryCurriculum(BaseCurriculum):
+    def __init__(self):
+        super().__init__(EmojiMysteryCurriculum.__name__, EmojiMysteryConfig)
+
+        self._define_attributes(
+            RangeAttributeDefinition(
+                name="num_words_in_sentence",
+                levels=[3, 10, 20, 35],
+                default_level=0,
+                description="Number of words in the sentence",
+                attr_type=AttributeType.STATIC,
+                min_value=3,
+                lower_field_name="min_words_in_sentence",
+                upper_field_name="max_words_in_sentence",
+            ),
+        )
 
 
 register_dataset("emoji_mystery", EmojiMysteryDataset, EmojiMysteryConfig)
