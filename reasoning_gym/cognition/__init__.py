082bb0cda41d121cc9c910e7a57940464d9e2d99
Zafir Stojanovski <zaf.stojano@gmail.com>
2025-03-13 18:24:54 +0100

    add curriculum to `__all__`

--- reasoning_gym/cognition/__init__.py
+++ reasoning_gym/cognition/__init__.py
@@ -25,6 +25,7 @@ __all__ = [
     "RectangleCountDataset",
     "NeedleHaystackConfig",
     "NeedleHaystackDataset",
+    "NeedleHaystackCurriculum",
     "ModuloGridConfig",
     "ModuloGridDataset",
 ]
