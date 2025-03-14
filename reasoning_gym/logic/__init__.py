408f1b1ea2b149c171e6a36a250cbbc8b659bf07
Zafir Stojanovski <zaf.stojano@gmail.com>
2025-03-14 12:46:32 +0100

    circuit logic curriculum

--- reasoning_gym/logic/__init__.py
+++ reasoning_gym/logic/__init__.py
@@ -3,7 +3,7 @@ Logic tasks for training reasoning capabilities.
 """
 
 from .aiw import AliceInWonderlandConfig, AliceInWonderlandCurriculum, AliceInWonderlandDataset
-from .circuit_logic import CircuitLogicConfig, CircuitLogicDataset
+from .circuit_logic import CircuitLogicConfig, CircuitLogicCurriculum, CircuitLogicDataset
 from .knights_knaves import KnightsKnavesConfig, KnightsKnavesDataset
 from .propositional_logic import PropositionalLogicConfig, PropositionalLogicDataset
 from .self_reference import SelfReferenceConfig, SelfReferenceCurriculum, SelfReferenceDataset
@@ -27,6 +27,7 @@ __all__ = [
     "SelfReferenceDataset",
     "CircuitLogicConfig",
     "CircuitLogicDataset",
+    "CircuitLogicCurriculum",
     "KnightsKnavesConfig",
     "KnightsKnavesDataset",
 ]
