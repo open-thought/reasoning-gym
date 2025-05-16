9e58ea9c304fe2ecd348ccd4200bbcac5ce36f9e
Zafir Stojanovski <zaf.stojano@gmail.com>
2025-05-16 12:44:56 +0200

    qwen math training code

---
+++ training/qwen-math/scripts/set/run_download_model.py
@@ -0,0 +1,10 @@
+from huggingface_hub import snapshot_download
+import os
+
+
+if __name__ == "__main__":
+    CKPT_DIR = os.environ['CKPT_DIR']
+
+    print("Downloading model ...")
+    snapshot_download(repo_id="Qwen/Qwen2.5-3B-Instruct",
+                      local_dir=f"{CKPT_DIR}/models/Qwen2.5-3B-Instruct/base")
